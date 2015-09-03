109f714c06b7381a5e1da984361d14f588672db5
kev_bite <kev_bite@msn.com>
2015-09-03 22:06:37 +0100

    Adding helper extension for configuring

---
+++ src/HumbleConfig.ConfigurationManager/ConfigurationManagerExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace HumbleConfig.ConfigurationManager
+{
+    public static class ConfigurationManagerExtensions
+    {
+        public static Configuration AddEnvironmentVariables(this Configuration configuration)
+        {
+            configuration.AddConfigurationSource(new ConfigurationManagerSource());
+
+            return configuration;
+        }
+    }
+}
