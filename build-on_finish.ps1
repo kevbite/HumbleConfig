24156667a62e50881de7222184d0390b560c30c4
kev_bite <kev_bite@msn.com>
2015-09-03 23:04:39 +0100

    Build config and nuspecs

---
+++ build-on_finish.ps1
@@ -0,0 +1,4 @@
+if ($env:DebugBuildWorker -like "true")
+{
+    $blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+}
--- src/HumbleConfig.EnvironmentVariables/Properties/AssemblyInfo.cs
+++ src/HumbleConfig.EnvironmentVariables/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HumbleConfig.EnvironmentVariables")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HumbleConfig")]
 [assembly: AssemblyProduct("HumbleConfig.EnvironmentVariables")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © HumbleConfig 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
