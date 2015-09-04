c3ed27ceb18d06aef31770ff2afbaf24c0f133a2
kev_bite <kev_bite@msn.com>
2015-09-04 14:03:48 +0100

    Adding a key prefixer formatter.

---
+++ src/HumbleConfig.Tests/KeyFormatters/KeyPrefixerTests.cs
@@ -0,0 +1,49 @@
+﻿using HumbleConfig.KeyFormatters;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace HumbleConfig.Tests.KeyFormatters
+{
+    public class KeyPrefixerTests
+    {
+        private KeyPrefixer _formatter;
+        private string _key;
+        private string _formattedKey;
+        private string _prefix;
+
+        [TestFixtureSetUp]
+        public void GivenAKeyPrefixer()
+        {
+            _prefix = new Fixture().Create<string>();
+            _formatter = new KeyPrefixer(_prefix);
+        }
+
+        [SetUp]
+        public void WhenFormattingTheKey()
+        {
+            _key = new Fixture().Create<string>();
+            _formattedKey = _formatter.FormatKey(_key);
+        }
+
+        [Test]
+        public void ThenTheKeyIsPrefixed()
+        {
+            Assert.That(_formattedKey, Is.StringStarting(_prefix));
+        }
+
+        [Test]
+        public void ThenTheFormattedKeyEndWithTheKey()
+        {
+            Assert.That(_formattedKey, Is.StringEnding(_key));
+        }
+
+
+        [Test]
+        public void ThenTheFormattedKeyIsTheCorrectLength()
+        {
+            var expectedLength = _key.Length + _prefix.Length;
+
+            Assert.That(_formattedKey.Length, Is.EqualTo(expectedLength));
+        }
+    }
+}
---
+++ src/HumbleConfig/KeyFormatters/KeyPrefixer.cs
@@ -0,0 +1,17 @@
+﻿namespace HumbleConfig.KeyFormatters
+{
+    public class KeyPrefixer : IKeyFormatter
+    {
+        private readonly string _keyPrefix;
+
+        public KeyPrefixer(string keyPrefix)
+        {
+            _keyPrefix = keyPrefix;
+        }
+
+        public string FormatKey(string key)
+        {
+            return string.Format($"{_keyPrefix}{key}");
+        }
+    }
+}
