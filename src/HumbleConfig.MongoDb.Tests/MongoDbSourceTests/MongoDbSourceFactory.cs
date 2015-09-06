bb0d38cd4917301f1e4c52e90d6120a320e46757
kevbite <kev_bite@msn.com>
2015-09-06 17:44:24 +0100

    Initial start of a mongodb source.

---
+++ src/HumbleConfig.MongoDb.Tests/MongoDbSourceTests/MongoDbSourceFactory.cs
@@ -0,0 +1,29 @@
+﻿using HumbleConfig.Tests;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace HumbleConfig.MongoDb.Tests.MongoDbSourceTests
+{
+    public class MongoDbSourceFactory : IConfigurationSourceFactory
+    {
+        public IConfigurationSource Create()
+        {
+            var collection = new MongoClient().GetDatabase("test").GetCollection<MongoDbSource.AppSetting>("appSettings");
+
+            return new MongoDbSource(collection);
+        }
+
+        public IConfigurationSource Create<TValue>(string key, TValue value)
+        {
+            var collection = new MongoClient().GetDatabase("test").GetCollection<MongoDbSource.AppSetting>("appSettings");
+            
+            collection.InsertOneAsync(new MongoDbSource.AppSetting()
+            {
+                 Id = key,
+                 Value = value
+            }).Wait();
+
+            return new MongoDbSource(collection);
+        }
+    }
+}
