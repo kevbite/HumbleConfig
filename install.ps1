4e9ddb9b9f866c511d5edcb42e6fb79e68ea0630
kevbite <kev_bite@msn.com>
2016-07-13 21:34:32 +0100

    updating to build all branches with a unreleased version number, also added powershell script to check if a tag build and to change the tag version to tag name.

--- install.ps1
+++ install.ps1
@@ -1,7 +1,13 @@
 Write-Host "Checking if pull request"
-
 if ([bool]$env:APPVEYOR_PULL_REQUEST_NUMBER)
 {
    Write-Host "Build is pull request - updating version"
-   Update-AppveyorBuild -Version "0.0.0-pull-request-$($env:APPVEYOR_PULL_REQUEST_NUMBER)-$($env:APPVEYOR_BUILD_NUMBER)"
+   Update-AppveyorBuild -Version "0.0.0-pull-request-$($env:APPVEYOR_PULL_REQUEST_NUMBER)"
+}
+
+Write-Host "Checking if tag"
+if ([bool]$env:APPVEYOR_REPO_TAG)
+{
+   Write-Host "Build is tag - updating version"
+   Update-AppveyorBuild -Version $env:APPVEYOR_REPO_TAG_NAME
 }
