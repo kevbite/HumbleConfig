78eed8e73db5f7b1660e9472646047bbb121833e
kevbite <kev_bite@msn.com>
2016-07-13 21:50:54 +0100

    Updated yml again just to skip tags.

--- install.ps1
+++ install.ps1
@@ -5,9 +5,3 @@ if ([bool]$env:APPVEYOR_PULL_REQUEST_NUMBER)
    Update-AppveyorBuild -Version "0.0.0-pull-request-$($env:APPVEYOR_PULL_REQUEST_NUMBER)"
 }
 
-Write-Host "Checking if tag"
-if ([bool]$env:APPVEYOR_REPO_TAG)
-{
-   Write-Host "Build is tag - updating version"
-   Update-AppveyorBuild -Version $env:APPVEYOR_REPO_TAG_NAME
-}
