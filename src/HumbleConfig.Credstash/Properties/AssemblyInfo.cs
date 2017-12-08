406bcc23cc9eb1af61262bf475df7aa19fce9af7
kevbite <kev_bite@msn.com>
2017-12-08 12:17:42 +0000

    HumbleConfig.Credstash.nuspec template.

--- src/HumbleConfig.Credstash/Properties/AssemblyInfo.cs
+++ src/HumbleConfig.Credstash/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HumbleConfig.Credstash")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HumbleConfig.Credstash")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("HumbleConfig")]
+[assembly: AssemblyProduct("HumbleConfig.MongoDb")]
+[assembly: AssemblyCopyright("Copyright © HumbleConfig 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
