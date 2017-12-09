2f573e94b6a4011529429b0e41ba4d3b6f54874e
kevbite <kev_bite@msn.com>
2017-12-09 14:12:10 +0000

    Fixing cache bug whereby the cache would use the same keys.

--- src/HumbleConfig.Caching/CachingExtentions.cs
+++ src/HumbleConfig.Caching/CachingExtentions.cs
@@ -6,22 +6,29 @@ namespace HumbleConfig.Caching
 {
     public static class CachingExtentions
     {
-        public static IConfigurationSourceConfigurator WithCache(this IConfigurationSourceConfigurator a, ObjectCache objectCache, Func<CacheItemPolicy> cacheItemPolicyFactory)
+        public static IConfigurationSourceConfigurator WithCache(this IConfigurationSourceConfigurator configurationSourceConfigurator, ICacheKeyCreator cacheKeyCreator, ObjectCache objectCache, Func<CacheItemPolicy> cacheItemPolicyFactory)
         {
-            return a.WrapSource(x => new CachingConfigurationSourceDecorator(x, objectCache, cacheItemPolicyFactory));
+            return configurationSourceConfigurator.WrapSource(x => new CachingConfigurationSourceDecorator(x, cacheKeyCreator, objectCache, cacheItemPolicyFactory));
         }
 
-        public static IConfigurationSourceConfigurator WithDefaultMemoryCache(this IConfigurationSourceConfigurator a, Func<CacheItemPolicy> cacheItemPolicyFactory)
+        public static IConfigurationSourceConfigurator WithCache(this IConfigurationSourceConfigurator configurationSourceConfigurator, ObjectCache objectCache, Func<CacheItemPolicy> cacheItemPolicyFactory)
         {
-            return a.WrapSource(x => new CachingConfigurationSourceDecorator(x, MemoryCache.Default, cacheItemPolicyFactory));
+            var cacheKeyCreator = new CacheKeyCreator(Guid.NewGuid().ToString());
+
+            return WithCache(configurationSourceConfigurator, cacheKeyCreator, objectCache, cacheItemPolicyFactory);
         }
 
-        public static IConfigurationSourceConfigurator WithDefaultMemoryCache(this IConfigurationSourceConfigurator a, TimeSpan expiresAfter)
+        public static IConfigurationSourceConfigurator WithDefaultMemoryCache(this IConfigurationSourceConfigurator configurationSourceConfigurator, Func<CacheItemPolicy> cacheItemPolicyFactory)
         {
-            return a.WrapSource(x => new CachingConfigurationSourceDecorator(x, MemoryCache.Default, () => new CacheItemPolicy()
+            return WithCache(configurationSourceConfigurator, MemoryCache.Default, cacheItemPolicyFactory);
+        }
+
+        public static IConfigurationSourceConfigurator WithDefaultMemoryCache(this IConfigurationSourceConfigurator configurationSourceConfigurator, TimeSpan expiresAfter)
+        {
+            return WithDefaultMemoryCache(configurationSourceConfigurator, () => new CacheItemPolicy()
             {
                 AbsoluteExpiration = DateTimeOffset.UtcNow.Add(expiresAfter)
-            }));
+            });
         }
     }
 }
--- src/HumbleConfig.FunctionalTests/CacheConfigurationTests.cs
+++ src/HumbleConfig.FunctionalTests/CacheConfigurationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Caching;
 using System.Threading.Tasks;
 using HumbleConfig.Caching;
 using HumbleConfig.InMemory;
@@ -28,8 +29,7 @@ namespace HumbleConfig.FunctionalTests
             };
 
             _configuration = new Configuration()
-                .AddInMemory(_inMemory)
-                .WithDefaultMemoryCache(TimeSpan.FromSeconds(1))
+                .AddInMemory(_inMemory).WithDefaultMemoryCache(TimeSpan.FromSeconds(1))
                 .GetConfiguration();
         }
 
@@ -43,6 +43,42 @@ namespace HumbleConfig.FunctionalTests
             var value2 = await _configuration.GetAppSettingAsync<string>(key1);
 
             Assert.AreEqual(value1, value2);
+        }
+    }
+
+    [TestFixture]
+    public class MultiCacheConfigurationTests
+    {
+        private IConfiguration _configuration;
+
+        private string _key1;
+        private Dictionary<string, object> _inMemory;
+        private Fixture _fixture;
+
+        [OneTimeSetUp]
+        public void GivenConfigurationWithEnvironmentVaribleAndConfigurationManager()
+        {
+            _fixture = new Fixture();
+            _key1 = _fixture.Create("key1");
+            _inMemory = new Dictionary<string, object>()
+            {
+                { _key1, "InMemory1"},
+            };
+
+            _configuration = new Configuration()
+                .AddInMemory(new Dictionary<string, object>()).WithDefaultMemoryCache(() => new CacheItemPolicy())
+                .AddInMemory(new Dictionary<string, object>{{ _key1, "InMemory1"}}).WithDefaultMemoryCache(() => new CacheItemPolicy())
+                .GetConfiguration();
+        }
+
+        [Test]
+        public async Task WhenChangingValueStillReturnsTheSameValue()
+        {
+            var value1 = await _configuration.GetAppSettingAsync<string>(_key1);
+            var value2 = await _configuration.GetAppSettingAsync<string>(_key1);
+
+            Assert.AreEqual(value1, "InMemory1");
+            Assert.AreEqual(value2, "InMemory1");
         }
     }
 }
