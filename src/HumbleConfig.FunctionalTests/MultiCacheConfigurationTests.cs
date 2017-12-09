cc8b0994b3df3193067d611552ebe8ae913bdfcf
kevbite <kev_bite@msn.com>
2017-12-09 14:21:17 +0000

    Moving to seperate files.

--- src/HumbleConfig.FunctionalTests/MultiCacheConfigurationTests.cs
+++ src/HumbleConfig.FunctionalTests/MultiCacheConfigurationTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Runtime.Caching;
 using System.Threading.Tasks;
 using HumbleConfig.Caching;
 using HumbleConfig.InMemory;
@@ -9,11 +9,11 @@ using Ploeh.AutoFixture;
 namespace HumbleConfig.FunctionalTests
 {
     [TestFixture]
-    public class CacheConfigurationTests
+    public class MultiCacheConfigurationTests
     {
         private IConfiguration _configuration;
 
-        private string key1;
+        private string _key1;
         private Dictionary<string, object> _inMemory;
         private Fixture _fixture;
 
@@ -21,27 +21,26 @@ namespace HumbleConfig.FunctionalTests
         public void GivenConfigurationWithEnvironmentVaribleAndConfigurationManager()
         {
             _fixture = new Fixture();
-            key1 = _fixture.Create("key1");
+            _key1 = _fixture.Create("key1");
             _inMemory = new Dictionary<string, object>()
             {
-                { key1, "InMemory1"},
+                { _key1, "InMemory1"},
             };
 
             _configuration = new Configuration()
-                .AddInMemory(_inMemory).WithDefaultMemoryCache(TimeSpan.FromSeconds(1))
+                .AddInMemory(new Dictionary<string, object>()).WithDefaultMemoryCache(() => new CacheItemPolicy())
+                .AddInMemory(new Dictionary<string, object>{{ _key1, "InMemory1"}}).WithDefaultMemoryCache(() => new CacheItemPolicy())
                 .GetConfiguration();
         }
 
         [Test]
         public async Task WhenChangingValueStillReturnsTheSameValue()
         {
-            var value1 = await _configuration.GetAppSettingAsync<string>(key1);
+            var value1 = await _configuration.GetAppSettingAsync<string>(_key1);
+            var value2 = await _configuration.GetAppSettingAsync<string>(_key1);
 
-            _inMemory[key1] = _fixture.Create("key1");
-
-            var value2 = await _configuration.GetAppSettingAsync<string>(key1);
-
-            Assert.AreEqual(value1, value2);
+            Assert.AreEqual(value1, "InMemory1");
+            Assert.AreEqual(value2, "InMemory1");
         }
     }
 }
